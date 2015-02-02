99ce9e7adb8fe12b3c46e3b791fbc36e91eba6de
Dylan McDonald <dmcdonald21@cnm.edu>
2015-02-02 11:15:42 -0700

    Added splash page

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet" />
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+		<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+		<title>Advanced Recursive Laravel Objects (ARLO)</title>
+	</head>
+	<body>
+		<main class="container">
+			<h1>Advanced Recursive Laravel Objects (ARLO)</h1>
+			<img class="img-responsive" src="images/arlo.jpg" alt="" />
+		</main>
+	</body>
+</html>
