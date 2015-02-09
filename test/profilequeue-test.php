f1ae80a990e9c43637b05f0f4b169bb6db79d723
Alonso Indacochea <aindaco1@hotmail.com>
2015-02-08 20:00:56 -0700

    update profilequeue test

--- test/profilequeue-test.php
+++ test/profilequeue-test.php
@@ -1,12 +1,8 @@
 <?php
-// first, require the SimpleTest framework <http://www.simpletest.org/>
-require_once("/usr/lib/php5/simpletest/autorun.php");
-// the class to test
+require_once('/usr/lib/php5/simpletest/autorun.php');
 require_once("../php/classes/profilequeue.php");
-// the classes required for foreign key access
 require_once ("../php/classes/profile.php");
 require_once ("../php/classes/queue.php");
-// require the encrypted configuration functions
 require_once("../lib/encrypted-config.php");
 /**
  * Unit test for the profileQueue class
@@ -38,7 +34,7 @@ class ProfileQueueTest extends UnitTestCase {
 		// connection
 		mysqli_report(MYSQLI_REPORT_STRICT);
 		$this->mysqli = new mysqli($configArray["hostname"], $configArray["username"], $configArray["password"],
-			$configArray["database"]);
+		$configArray["database"]);
 		// instance of objects under scrutiny
 		$date = new DateTime();
 		$this->profile = new Profile(null, "test@test.com", 'http://www.cats.com/cat.jpg');
@@ -72,95 +68,95 @@ class ProfileQueueTest extends UnitTestCase {
 			}
 		}
 	}
-		/**
-		 * test inserting a valid ProfileQueue into mySQL
-		 **/
+	/**
+	 * test inserting a valid ProfileQueue into mySQL
+	 **/
 	public function testInsertValidProfileQueue() {
-			// zeroth, ensure the ProfileQueue and mySQL class are sane
-			$this->assertNotNull($this->profileQueue);
-			$this->assertNotNull($this->mysqli);
-			// first, insert the ProfileQueue into mySQL
-			$this->profileQueue->insert($this->mysqli);
-			// second, grab a ProfileQueue from mySQL
-			$mysqlProfileQueue = ProfileQueue::getProfileQueueByProfileIdAndQueueId($this->mysqli, $this->profileQueue->getProfileId(), $this->profileQueue->getQueueId());
-			// third, assert the ProfileQueue we have created and mySQL's ProfileQueue are the same object
+		// zeroth, ensure the ProfileQueue and mySQL class are sane
+		$this->assertNotNull($this->profileQueue);
+		$this->assertNotNull($this->mysqli);
+		// first, insert the ProfileQueue into mySQL
+		$this->profileQueue->insert($this->mysqli);
+		// second, grab a ProfileQueue from mySQL
+		$mysqlProfileQueue = ProfileQueue::getProfileQueueByProfileIdAndQueueId($this->mysqli, $this->profileQueue->getProfileId(), $this->profileQueue->getQueueId());
+		// third, assert the ProfileQueue we have created and mySQL's ProfileQueue are the same object
+		$this->assertIdentical($this->profileQueue->getProfileId(), $mysqlProfileQueue->getProfileId());
+		$this->assertIdentical($this->profileQueue->getQueueId(), $mysqlProfileQueue->getQueueId());
+	}
+
+	/**
+	 * test inserting an invalid ProfileQueue into mySQL
+	 **/
+	public function testInsertInvalidProfileQueue() {
+		// zeroth, ensure the ProfileQueue and mySQL class are sane
+		$this->assertNotNull($this->profileQueue);
+		$this->assertNotNull($this->mysqli);
+		// first, set the profile id and queue id to an invented value that should never insert in the first place
+		$this->profileQueue->setProfileId(42);
+		$this->profileQueue->setQueueId(42);
+		// second, try to insert the ProfileQueue and ensure the exception is thrown
+		$this->expectException("mysqli_sql_exception");
+		$this->profileQueue->insert($this->mysqli);
+		// third, set the ProfileQueue to null to prevent tearDown() from deleting a ProfileQueue that never existed
+		$this->profileQueue = null;
+	}
+
+	/**
+	 * test deleting a ProfileQueue from mySQL
+	 **/
+	public function testDeleteValidProfileQueue() {
+		// zeroth, ensure the ProfileQueue and mySQL class are sane
+		$this->assertNotNull($this->profileQueue);
+		$this->assertNotNull($this->mysqli);
+		// first, assert the ProfileQueue is inserted into mySQL by grabbing it from mySQL and asserting the primary key
+		$this->profileQueue->insert($this->mysqli);
+		$mysqlProfileQueue = ProfileQueue::getProfileQueueByProfileIdAndQueueId($this->mysqli, $this->profileQueue->getProfileId(), $this->profileQueue->getQueueId());
+		$this->assertIdentical($this->profileQueue->getProfileId(), $mysqlProfileQueue->getProfileId());
+		$this->assertIdentical($this->profileQueue->getQueueId(), $mysqlProfileQueue->getQueueId());
+		// second, delete the ProfileQueue from mySQL and re-grab it from mySQL and assert it does not exist
+		$this->profileQueue->delete($this->mysqli);
+		$mysqlProfileQueue = ProfileQueue::getProfileQueueByProfileIdAndQueueId($this->mysqli, $this->profileQueue->getProfileId(), $this->profileQueue->getQueueId());
+		$this->assertNull($mysqlProfileQueue);
+		// third, set the ProfileQueue to null to prevent tearDown() from deleting a ProfileQueue that has already been deleted
+		$this->profileQueue = null;
+		}
+
+	/**
+	 * test get ProfileQueue by valid profile id and valid queue id
+	 */
+	public function testGetProfileQueueByValidProfileIdAndValidQueueId() {
+		$this->assertNotNull($this->profileQueue);
+		$this->assertNotNull($this->mysqli);
+		// first, insert the ProfileQueue into mySQL
+		$this->profileQueue->insert($this->mysqli);
+		// second, grab the ProfileQueue from mySQL
+		$mysqlProfileQueues = ProfileQueue::getProfileQueueByProfileIdAndQueueId($this->mysqli, $this->profileQueue->getProfileId(), $this->profileQueue->getQueueId());
+		// third, assert the ProfileQueue we have created and mySQL's ProfileQueue are the same object
+		foreach($mysqlProfileQueues as $mysqlProfileQueue) {
+			$this->assertNotNull($mysqlProfileQueue->getProfileId());
+			$this->assertTrue($mysqlProfileQueue->getProfileId() > 0);
+			$this->assertNotNull($mysqlProfileQueue->getQueueId());
+			$this->assertTrue($mysqlProfileQueue->getQueueId() > 0);
 			$this->assertIdentical($this->profileQueue->getProfileId(), $mysqlProfileQueue->getProfileId());
 			$this->assertIdentical($this->profileQueue->getQueueId(), $mysqlProfileQueue->getQueueId());
 		}
+	}
 
-		/**
-		 * test inserting an invalid ProfileQueue into mySQL
-		 **/
-		public function testInsertInvalidProfileQueue() {
-			// zeroth, ensure the ProfileQueue and mySQL class are sane
-			$this->assertNotNull($this->profileQueue);
-			$this->assertNotNull($this->mysqli);
-			// first, set the profile id and queue id to an invented value that should never insert in the first place
-			$this->profileQueue->setProfileId(42);
-			$this->profileQueue->setQueueId(42);
-			// second, try to insert the ProfileQueue and ensure the exception is thrown
-			$this->expectException("mysqli_sql_exception");
-			$this->profileQueue->insert($this->mysqli);
-			// third, set the ProfileQueue to null to prevent tearDown() from deleting a ProfileQueue that never existed
-			$this->profileQueue = null;
-		}
+	/**
+	 * test get profile queue by invalid profile id or invalid queue id
+	 */
+	public function testGetProfileQueueByInvalidProfileIdAndInvalidQueueId() {
+		$this->assertNotNull($this->profileQueue);
+		$this->assertNotNull($this->mysqli);
 
-		/**
-		 * test deleting a ProfileQueue from mySQL
-		 **/
-		public function testDeleteValidProfileQueue() {
-			// zeroth, ensure the ProfileQueue and mySQL class are sane
-			$this->assertNotNull($this->profileQueue);
-			$this->assertNotNull($this->mysqli);
-			// first, assert the ProfileQueue is inserted into mySQL by grabbing it from mySQL and asserting the primary key
-			$this->profileQueue->insert($this->mysqli);
-			$mysqlProfileQueue = ProfileQueue::getProfileQueueByProfileIdAndQueueId($this->mysqli, $this->profileQueue->getProfileId(), $this->profileQueue->getQueueId());
-			$this->assertIdentical($this->profileQueue->getProfileId(), $mysqlProfileQueue->getProfileId());
-			$this->assertIdentical($this->profileQueue->getQueueId(), $mysqlProfileQueue->getQueueId());
-			// second, delete the ProfileQueue from mySQL and re-grab it from mySQL and assert it does not exist
-			$this->profileQueue->delete($this->mysqli);
-			$mysqlProfileQueue = ProfileQueue::getProfileQueueByProfileIdAndQueueId($this->mysqli, $this->profileQueue->getProfileId(), $this->profileQueue->getQueueId());
-			$this->assertNull($mysqlProfileQueue);
-			// third, set the ProfileQueue to null to prevent tearDown() from deleting a ProfileQueue that has already been deleted
-			$this->profileQueue = null;
-		}
+		// first, insert the profileQueue into mySQL
+		$this->profileQueue->insert($this->mysqli);
 
-		/**
-		 * test get ProfileQueue by valid profile id and valid queue id
-		 */
-		public function testGetProfileQueueByValidProfileIdAndValidQueueId() {
-			$this->assertNotNull($this->profileQueue);
-			$this->assertNotNull($this->mysqli);
-			// first, insert the ProfileQueue into mySQL
-			$this->profileQueue->insert($this->mysqli);
-			// second, grab the ProfileQueue from mySQL
-			$mysqlProfileQueues = ProfileQueue::getProfileQueueByProfileIdAndQueueId($this->mysqli, $this->profileQueue->getProfileId(), $this->profileQueue->getQueueId());
-			// third, assert the ProfileQueue we have created and mySQL's ProfileQueue are the same object
-			foreach($mysqlProfileQueues as $mysqlProfileQueue) {
-				$this->assertNotNull($mysqlProfileQueue->getProfileId());
-				$this->assertTrue($mysqlProfileQueue->getProfileId() > 0);
-				$this->assertNotNull($mysqlProfileQueue->getQueueId());
-				$this->assertTrue($mysqlProfileQueue->getQueueId() > 0);
-				$this->assertIdentical($this->profileQueue->getProfileId(), $mysqlProfileQueue->getProfileId());
-				$this->assertIdentical($this->profileQueue->getQueueId(), $mysqlProfileQueue->getQueueId());
-			}
-		}
+		// second, create an array grabbing a profileQueue that doesn't exist
+		$mysqlProfileQueues = ProfileQueue::getProfileQueueByProfileIdAndQueueId($this->mysqli, 100, 101);
 
-		/**
-		 * test get profile queue by invalid profile id or invalid queue id
-		 */
-		public function testGetProfileQueueByInvalidProfileIdAndInvalidQueueId() {
-			$this->assertNotNull($this->profileQueue);
-			$this->assertNotNull($this->mysqli);
-
-			// first, insert the profileQueue into mySQL
-			$this->profileQueue->insert($this->mysqli);
-
-			// second, create an array grabbing a profileQueue that doesn't exist
-			$mysqlProfileQueues = ProfileQueue::getProfileQueueByProfileIdAndQueueId($this->mysqli, 100, 101);
-
-			// third, assert the array is null
-			$this->assertNull($mysqlProfileQueues);
-		}
+		// third, assert the array is null
+		$this->assertNull($mysqlProfileQueues);
+	}
 }
 ?>
