f72978a72281f35b73861cfb112368556baea941
James Mistalski <kimomistalski@gmail.com>
2015-02-10 00:23:25 -0700

    continuing working on profile-test.php https://app.asana.com/0/25697096870154/25697096870164

--- test/profile-test.php
+++ test/profile-test.php
@@ -27,8 +27,8 @@ class ProfileTest extends unitTestCase {
 	/**
 	 * instance of the objects we are testing with
 	 **/
-	private $email	= null;
-	private $imagePath = null;
+	private $email	= "unit-test@example.net";
+	private $imagePath = "http://placehold.it/350x150";
 
 	/**
 	 * sets up the mySQL connection for this test
@@ -41,7 +41,46 @@ class ProfileTest extends unitTestCase {
 		mysqli_report(MYSQLI_REPORT_STRICT);
 		$this->mysqli = new mysqli($configArray["hostname"], $configArray["username"], $configArray["password"],
 			$configArray["database"]);
+	}
 
-		
+	/**
+	 * tears down the connection to mySQL and deletes the test instance object
+	 **/
+	public function tearDown() {
+		// destroy the objects if they were created
+		if($this->profile !== null) {
+			$this->profile->delete($this->mysqli);
+			$this->profile = null;
+
+			// disconnect from mySQL
+			if($this->mysqli !== null) {
+				$this->mysqli->close();
+				$this->mysqli = null;
+			}
+		}
+	}
+	/**
+	 * test creating a new Profile and inserting it into mySQL
+	 **/
+	public function testInsertNewProfile() {
+		// zeroth, test mySQL and Profile are sane
+		$this->assertNotNull($this->mysqli);
+		$this->assertNotNull($this->profile);
+		// first, create a profile to post to mySQL
+		$this->profile = new Profile(null, $this->email, $this->imagePath);
+		// second, insert the profile to mySQL
+		$this->profile->insert($this->mysqli);
+		// finally, compare the fields
+		$this->assertNotNull($this->profile->getProfileId());
+		$this->assertTrue($this->profile->getProfileId() > 0);
+		$this->assertIdentical($this->profile->getEmail(),			$this->email);
+		$this->assertIdentical($this->profile->getImagePath(),	$this->imagePath);
+	}
+
+	/**
+	 * test updating a Profile in mySQL
+	 **/
+	
 
 }
+?>
