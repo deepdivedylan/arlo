b29319ec81f93cf51be801521adea6d6b650cf82
James Mistalski <kimomistalski@gmail.com>
2015-02-11 01:56:45 -0700

    added test suite to run all tests at one forward, backward, and random - https://app.asana.com/0/25697096870154/25697096870164

---
+++ php/classes/arlo-suite.php
@@ -0,0 +1,48 @@
+<?php
+// 1st require the SimpleTest framework
+require_once("/usr/lib/php5/simpletest/autorun.php");
+
+class ArloSuite extends TestSuite {
+
+	/**
+	 * mysqli object shared amongst all tests
+	 */
+	private $mysqli = null;
+
+	public function setUp() {
+		// get the credentials information from the server
+		$configFile = "/etc/apache2/arlo.ini";
+		$configArray = readConfig($configFile);
+		// connection
+		mysqli_report(MYSQLI_REPORT_STRICT);
+		$this->mysqli = new mysqli($configArray["hostname"], $configArray["username"], $configArray["password"],
+			$configArray["database"]);
+	}
+	
+	// the constructor for a TestSuite just sets up all the file names
+	public function __construct() {
+		// run the parent constructor
+		parent::__construct();
+
+		// stuff the test files into an array
+		// TODO: add the files in the "forward" order
+		$testFiles = array("profile.php", "queue.php", "video.php", "profilequeue.php", "videoqueue.php");
+
+		// run them forward
+		foreach($testFiles as $testFile) {
+			$this->addFile($testFile);
+		}
+
+		// run them backward
+		$testFiles = array_reverse($testFiles, false);
+		foreach($testFiles as $testFile) {
+			$this->addFile($testFile);
+		}
+
+		// run them randomly
+		shuffle($testFiles);
+		foreach($testFiles as $testFile) {
+			$this->addFile($testFile);
+		}
+	}
+}
