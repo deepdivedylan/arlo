b39380180fd52452c4d3771de96a73884eb4fe3c
James Mistalski <kimomistalski@gmail.com>
2015-02-11 00:20:51 -0700

    started queue-test.php https://app.asana.com/0/25697096870154/25697096870164

--- test/queue-test.php
+++ test/queue-test.php
@@ -1,7 +1,31 @@
 <?php
+require_once('/usr/lib/php5/simpletest/autorun.php');
+require_once ("../php/classes/queue.php");
+require_once("../lib/encrypted-config.php");
 /**
- * Created by PhpStorm.
- * User: alonso
- * Date: 2/8/15
- * Time: 5:32 PM
- */
+ *
+ * Unit test for the queue class
+ *
+ * This is a SimpleTest test case for the CRUD methods of the queue class.
+ *
+ * @see queue
+ * @author James Mistalski <james.mistalski@gmail.com>
+ **/
+
+// the QueueTest is a container for all our tests
+class QueueTest extends unitTestCase {
+	/**
+	 * mysqli object shared amongst all tests
+	 */
+	private $mysqli = null;
+
+	/**
+	 * variable to hold the test database row
+	 */
+	private $queue = null;
+
+	/**
+	 * instance of the objects we are testing with
+	 */
+	private $creationDate = ""
+}
