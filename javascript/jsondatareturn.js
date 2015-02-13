81136577ac3f4fea769b6a36a3cb63e4c91bf176
James Mistalski <kimomistalski@gmail.com>
2015-02-12 21:30:12 -0700

    adding json return - https://app.asana.com/0/25697096870154/25697096870164

--- javascript/jsondatareturn.js
+++ javascript/jsondatareturn.js
@@ -4,6 +4,8 @@
  * @param jsonString string containing JSON results
  * @return string HTML formatted data
  **/
+
+
 function displayResults(jsonString) {
 	var htmlResults = "";
 	var allResults = JSON.parse(jsonString);
@@ -20,7 +22,8 @@ function displayResults(jsonString) {
 
 	return(htmlResults);
 }
-
+$(document).ready(function()	{
 var json = "[{\"id\":\"16974\",\"name\":\"Team America: World Police\",\"plot\":\"\",\"imdbId\":\"0372588\",\"actors\":[\"Trey Parker\",\"Matt Stone\",\"Kristen Miller\",\"Masasa\",\"Daran Norris\"],\"banners\":[\"http://content6.flixster.com/movie/10/92/10/10921012_tmb.jpg\",\"http://content6.flixster.com/movie/10/92/10/10921012_tmb.jpg\",\"http://content6.flixster.com/movie/10/92/10/10921012_tmb.jpg\",\"http://content6.flixster.com/movie/10/92/10/10921012_tmb.jpg\"],\"stream\":\"http://www.canistream.it/external/imdb/0372588?l=default\"},{\"id\":3989,\"name\":\"Team America: World Police\",\"imdbId\":\"tt0372588\",\"plot\":\"Team America World Police follows an international police force dedicated to maintaining global stability. Learning that dictator Kim Jong il is out to destroy the world, the team recruits Broadway star Gary Johnston to go undercover. With the help of Team America, Gary manages to uncover the plan to destroy the world. Will Team America be able to save it in time? It stars? Samuel L Jackson, Tim Robbins, Sean Penn, Michael Moore, Helen Hunt, Matt Damon, Susan Sarandon, George Clooney, Danny Glover, " + "Ethan Hawke, Alec Baldwin? or does it?\",\"banners\":[\"/crkPGNm0M19LHp8I5mhs75EqhSH.jpg\"],\"stream\":\"http://www.canistream.it/external/imdb/tt0372588?l=default\",\"actors\":[\"Trey Parker\",\"Matt Stone\",\"Kristen Miller\",\"Masasa Moyo\",\"Daran Norris\",\"Maurice LaMarche\"]}]";
 
 document.getElementById("resultsData").innerHTML = displayResults(json);
+})
